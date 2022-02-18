3d3efcbe4917d1c03e0169d720569c1d3bd267f0
Hathamee <hathamee@berkeley.edu>
2022-02-17 19:40:03 -0800

    working on JS more in live demo

---
+++ L4-JS1/script.js
@@ -0,0 +1,9 @@
+function printHome(city, state, country) {
+    console.log(`I am from ${city}, ${state}, and ${country} !`)
+}
+
+var city = 'Roseville';
+var state = 'CA';
+var country = 'USA';
+
+printHome(city, state, country)
